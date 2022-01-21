da8d5ea70d6b97deaa692e266f5f617b5bb8b4fa
Martí Bosch <marti.bosch@epfl.ch>
2022-01-21 13:36:31 +0100

    initial commit

---
+++ acl_frontend_test/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from acl_frontend_test.users.models import User
+from acl_frontend_test.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
